6ce9052250d71879a02fd138960eaa643cd768ab
kpls <130404545+kpls-git@users.noreply.github.com>
2023-04-28 20:17:11 +0100

    Add files via upload

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,19 +6,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" href="/logo.jpg" />
+          <link rel="apple-touch-icon" href="/logo.png" />
           <meta name="robots" content="index,follow" />
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="The new and improved home for all the dedicated players of Black Squad!"
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@BlackSquadHub" />
+          <meta name="twitter:creator" content="@BlackSquadHub" />
+          <meta property="og:url" content="https://blacksquadhub.com" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Black Squad Hub" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,17 +26,17 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="The new and improved home for all the dedicated players of Black Squad"
           />
-          <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image" content="/img/logo.png" />
+          <meta property="og:image:alt" content="Black Squad Hub" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Black Squad Hub"
           />
           <meta name="theme-color" content="#6108dd" />
-          <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
+          <link rel="icon" href="/img/logo2.png" type="image/x-icon" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
